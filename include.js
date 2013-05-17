5e45d52abdd3b512786646cfcefc77c41fab18ef
yaopiaohai <yaopiaohai@corp.netease.com>
2013-05-18 01:02:36 +0800

    misc:init

---
+++ include.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+var _ndump = require('./build/Release/ndump');
+
+ndump = {};
+
+ndump.heap = function(file){
+	_ndump.heap.writeSnapshot(file);
+}
+
+ndump.cpu = function(file,times){
+	_ndump.cpu.start(file);
+	setTimeout(function(){
+		fs.writeFileSync(file,JSON.stringify(_ndump.cpu.stop(file)));
+	},times)
+}
+
+module.exports = ndump;
